f61edef66eaeaa7a49357900f918ba4bf36221e2
diego <diegoalcayetano@gmail.com>
2025-06-09 21:24:57 -0500

    analytics vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/components/theme-provider";
 import { IntlProvider } from 'next-intl';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <IntlProvider messages={pageProps.messages} locale={pageProps.locale}>
         <Component {...pageProps} />
+        <Analytics/>
       </IntlProvider>
     </ThemeProvider>
   )
