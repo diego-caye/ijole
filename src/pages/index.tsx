edd872ef67fcc9ceef4a4df9502a4bbeac079b80
diego <diegoalcayetano@gmail.com>
2025-06-09 21:28:41 -0500

    seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import SavedImages from "@/components/SavedImages";
 import { useState } from "react";
 import { GetStaticPropsContext } from "next";
 import LanguageSwitcher from "@/components/LanguageSwitcher";
-
+import Head from "next/head";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,20 +21,26 @@ export default function Home() {
   const [refreshCounter, setRefreshCounter] = useState(0);
 
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pt-8 md:p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]`}
-    >
-     
-      <main className="gap-[32px] row-start-2 items-center sm:items-start px-12 md:px-0">
-        <div className="p-9 flex justify-center gap-8">
-        <LanguageSwitcher />
-        <ModeToggle />
-        </div>
-        <FabricCanvas onSaved={() => setRefreshCounter((c) => c + 1)} />
+    <>
+      <Head>
+        <title>Ijole Generator / Edit Any Image</title>
+        <meta name="description" content="Image generator made for quick editing of various images or funny things." />
+      </Head>
+      <div
+        className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pt-8 md:p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]`}
+      >
 
-        <SavedImages refreshTrigger={refreshCounter} />
-      </main>
-    </div>
+        <main className="gap-[32px] row-start-2 items-center sm:items-start px-12 md:px-0">
+          <div className="p-9 flex justify-center gap-8">
+            <LanguageSwitcher />
+            <ModeToggle />
+          </div>
+          <FabricCanvas onSaved={() => setRefreshCounter((c) => c + 1)} />
+
+          <SavedImages refreshTrigger={refreshCounter} />
+        </main>
+      </div>
+    </>
   );
 }
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
